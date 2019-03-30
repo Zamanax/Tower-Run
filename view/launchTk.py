a7eed10e96f41c73c7695cb63691b9ce3dd72f7d
Maxime <zamax27@gmail.com>
2019-03-30 23:17:53 +0100

    Annotation de launchTk et nouvelles images

--- view/launchTk.py
+++ view/launchTk.py
@@ -1,35 +1,42 @@
 import tkinter as tk
 backImg = ""
-rootWidth = 0
-rootHeight = 0
 
+#-----------------Chargement de la Frame LVL 1 ----------------------
 class Lvl1(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
-        global backImg, rootHeight, rootWidth
+        # Définiton des variables
+        global backImg
+        rootWidth = backImg.width()
+        rootHeight = backImg.height()
+
+        # Instance de la Frame
         tk.Frame.__init__(self, parent)
         self.parent = parent
 
+        # Reste du GUI
         canvas = tk.Canvas(self, width=rootWidth,height=rootHeight,highlightthickness=0)
         canvas.create_image(0,0,image=backImg,anchor="nw")
         canvas.pack(side="right",fill="both",expand="true")
 
 
+#-----------------Chargement de la vue principale--------------------
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
+        # Instance de la Frame
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
+        # Chargement des Frames voulues
         self.lvl1 = Lvl1(self, parent)
 
+        # Mise en vue principale des vues voulues
         self.lvl1.pack()
 
 
 def launchApp():
-    global backImg, rootHeight, rootWidth
+    global backImg
     root = tk.Tk()
     backImg = tk.PhotoImage(file="./src/Lvl1Background.png")
-    rootWidth=backImg.width()
-    rootHeight=backImg.height()
     MainApplication(root).pack(side="top", fill="both", expand=True)
     root.title("Tower Defense")
     root.resizable(False,False)
