559363c04f8bb333c7a837c742a79138fdd0c4f3
Maxime <zamax27@gmail.com>
2019-04-07 00:14:12 +0200

    Singleton pattern pour le héros

--- model/Heros.py
+++ model/Heros.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 from model.Character import Character
 import model.Ennemy as Enn
-class Heros(Character):
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Heros(Character, metaclass=Singleton):
     # Stats du Héros
     team = "ally"
     hp = 30
